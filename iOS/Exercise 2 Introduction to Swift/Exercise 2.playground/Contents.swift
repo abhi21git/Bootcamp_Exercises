b8929f8afae41cbe3c16a7a5db19db0e6ca89a48
Abhishek Maurya <abhishek.maurya08@tothenew.com>
2019-02-19 22:01:05 +0530

    Added ex2

---
+++ iOS/Exercise 2 Introduction to Swift/Exercise 2.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "Exercise 2: Introduction to Swift"
+
+struct Personal {
+    var employeeID = [124, 154, 134, 129, 112, 139, 150, 188]
+    var name = ["Akaash", "Aditya", "Sweta", "Abhishek"]
+    var country = ["America", "India"]
+}
