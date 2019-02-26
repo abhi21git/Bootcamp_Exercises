def4bdfb1addfaed0b83c6a4038425ae21bffbd5
Abhishek Maurya <abhishek.maurya08@tothenew.com>
2019-02-26 18:53:33 +0530

    ex 3

--- iOS/Exercise 4 Swift Intermediate /Exercise 4 Swift Intermediate.playground/Contents.swift
+++ iOS/Exercise 4 Swift Intermediate /Exercise 4 Swift Intermediate.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
 var str = "Exercise 4: Swift Intermediate"
+
+//Question 1: Write a function called siftBeans(fromGroceryList:) that takes a grocery list (as an array of strings) and “sifts out” the beans from the other groceries. The function should take one argument that has a parameter name called list, and it should return a named tuple of the type (beans: [String], otherGroceries: [String]).
+var list : [String] = ["green beans", "banana", "black beans", "grapes", "red beans", "apples", "carrot"]
+func siftBeans( fromGroceryList : [String] ) -> (beans : [String] , otherGroceries :[String]) {
+    var beans = [String]()
+    var otherGroc = [String]()
+    for groceryItem in list {
+        if (groceryItem.hasSuffix("beans")) {
+            beans.append(groceryItem)
+        }
+        else {
+            otherGroc.append(groceryItem)
+        }
+    }
+    return (beans , otherGroc)
+}
+var  result = siftBeans(fromGroceryList: list)
+print(result.beans,"are beans.")
+print(result.otherGroceries,"are other groceries")
