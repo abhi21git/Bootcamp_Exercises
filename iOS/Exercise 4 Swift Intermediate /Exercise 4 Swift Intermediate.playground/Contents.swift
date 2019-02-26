54934e7aebf5a148c84198831d8467ef4cfb1e40
Abhishek Maurya <abhishek.maurya08@tothenew.com>
2019-02-26 23:11:45 +0530

    Ex4

--- iOS/Exercise 4 Swift Intermediate /Exercise 4 Swift Intermediate.playground/Contents.swift
+++ iOS/Exercise 4 Swift Intermediate /Exercise 4 Swift Intermediate.playground/Contents.swift
@@ -6,17 +6,49 @@ var str = "Exercise 4: Swift Intermediate"
 var list : [String] = ["green beans", "banana", "black beans", "grapes", "red beans", "apples", "carrot"]
 func siftBeans( fromGroceryList : [String] ) -> (beans : [String] , otherGroceries :[String]) {
     var beans = [String]()
-    var otherGroc = [String]()
+    var otherGrocery = [String]()
     for groceryItem in list {
         if (groceryItem.hasSuffix("beans")) {
             beans.append(groceryItem)
         }
         else {
-            otherGroc.append(groceryItem)
+            otherGrocery.append(groceryItem)
         }
     }
-    return (beans , otherGroc)
+    return (beans , otherGrocery)
 }
 var  result = siftBeans(fromGroceryList: list)
 print(result.beans,"are beans.")
 print(result.otherGroceries,"are other groceries")
+
+//Question 2.1: Make a calculator class with a function name “equals” that take a enum case as value like multiply, subtraction, addition, square root, division.
+indirect enum Calculator {
+    case number(Float)
+    case addition(Calculator, Calculator)
+    case substraction(Calculator, Calculator)
+    case multiplication(Calculator, Calculator)
+    case division(Calculator, Calculator)
+    case sqroot(Calculator)
+}
+func equals(_ operation: Calculator) -> Float {
+    switch operation {
+    case let .number(value):
+        return value
+    case let .addition(num1, num2):
+        return equals(num1) + equals(num2)
+    case let .substraction(num1, num2):
+        return equals(num1) - equals(num2)
+    case let .multiplication(num1, num2):
+        return equals(num1) * equals(num2)
+    case let .division(num1, num2):
+        return equals(num1) / equals(num2)
+    case let .sqroot(num1):
+        return sqrt(equals(num1))
+    }
+}
+var num1 = (Calculator.number(25))
+var num2 = (Calculator.number(5))
+print("\nFor \(num1) and \(num2)\nAddition is \(equals(Calculator.addition(num1, num2)))\nSubtraaction is \(equals(Calculator.substraction(num1, num2)))\nMultiplication is \(equals(Calculator.multiplication(num1, num2)))\nDivision is \(equals(Calculator.division(num1, num2)))\nSquare root is \(equals(Calculator.sqroot(num1)))")
+
+//Question 2.2: Make same calculator using functions as argument, define all type functions in a struct.
+
