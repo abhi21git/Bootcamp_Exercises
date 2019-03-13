6fe4215f1d127bbdf3868362ddce5560c75bfeb6
Abhishek Maurya <abhishek.maurya08@tothenew.com>
2019-03-13 15:25:18 +0530

    modified ex

--- iOS/Exercise 9 UI Elements/Exercise 9 UI Elements/ViewController.swift
+++ iOS/Exercise 9 UI Elements/Exercise 9 UI Elements/ViewController.swift
@@ -11,45 +11,32 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet var profilePicture: UIImageView!
-    @IBOutlet var profilePictureSelector: UIButton!
+    @IBOutlet var profilePictureSelectorButton: UIButton!
     @IBOutlet var date: UITextField!
     @IBOutlet var datePicker: UIDatePicker!
     
+    let profilePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        profilePicker.delegate = self
         profilePicture.layer.cornerRadius = profilePicture.frame.size.height/2
         profilePicture.layer.masksToBounds = true
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func uploadPic(_ sender: Any) {
-        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (alert: UIAlertAction!) -> Void in
-            self.camera()
-        } ))
-        actionSheet.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { (alert: UIAlertAction!) -> Void in
-            self.gallary()
-        } ))
-        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (alert: UIAlertAction!) -> Void in
-            self.present(actionSheet, animated:  true, completion:  nil)
-        } ))
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        profilePicture.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
     
-    func camera() {
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let cameraPickerCOntroller = UIImagePickerController()
-            cameraPickerCOntroller.delegate = self
-            cameraPickerCOntroller.sourceType = .camera
-            self.present(cameraPickerCOntroller, animated: true, completion: nil)
-        }
-    }
-    func gallary() {
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            let gallaryPickerCOntroller = UIImagePickerController()
-            gallaryPickerCOntroller.delegate = self
-            gallaryPickerCOntroller.sourceType = .photoLibrary
-            self.present(gallaryPickerCOntroller, animated: true, completion: nil)
-        }
+    @IBAction func uploadPicture(_ sender: Any) {
+        profilePicker.allowsEditing = false
+        profilePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        self.present(profilePicker, animated:  true, completion:  nil)
     }
 
     @IBAction func SignUpDone() {
