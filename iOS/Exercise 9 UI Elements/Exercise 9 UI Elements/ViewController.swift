00505817db2b92712911bbf0a828b747f5649ad1
Abhishek Maurya <abhishek.maurya08@tothenew.com>
2019-03-13 21:40:47 +0530

    modified exercise 9

--- iOS/Exercise 9 UI Elements/Exercise 9 UI Elements/ViewController.swift
+++ iOS/Exercise 9 UI Elements/Exercise 9 UI Elements/ViewController.swift
@@ -8,23 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet var profilePicture: UIImageView!
     @IBOutlet var profilePictureSelectorButton: UIButton!
-    @IBOutlet var date: UITextField!
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var dateOfBirthTextField: UITextField!
+    @IBOutlet var eMailtextField: UITextField!
+    @IBOutlet var placeOfBirthTextField: UITextField!
+    @IBOutlet var phoneNumberTextField: UITextField!
     @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var placePicker: UIPickerView!
+    @IBOutlet var privateMsgLabel: UILabel!
+    @IBOutlet var privateAccountSwitch: UISwitch!
     
     let profilePicker = UIImagePickerController()
+    let citiesName = ["New Delhi", "Noida", "Greater Noida", "Ghaziabad", "Lucknow", "Mumbai", "Kolkata","Chennai"]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        datePicker.isHidden = true
+        placePicker.isHidden = true
+        
+        //Code to make profile picture round
         profilePicker.delegate = self
         profilePicture.layer.cornerRadius = profilePicture.frame.size.height/2
         profilePicture.layer.masksToBounds = true
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.placePicker.delegate = self
+        self.placePicker.dataSource = self
+
+        userNameTextField.becomeFirstResponder()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    // Protocol for image picker
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         profilePicture.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         dismiss(animated: true, completion: nil)
@@ -33,12 +54,82 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true, completion: nil)
     }
     
+    // Function for opening photo library on click
     @IBAction func uploadPicture(_ sender: Any) {
         profilePicker.allowsEditing = false
         profilePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
         self.present(profilePicker, animated:  true, completion:  nil)
     }
-
+    
+    // Function for picking date if picker is scrolled
+    @IBAction func pickingDate(){
+        let selectedDate = datePicker.date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        let date = dateFormatter.string(from: selectedDate)
+        dateOfBirthTextField.text = date
+        datePicker.isHidden = true
+        eMailtextField.becomeFirstResponder()
+    }
+    
+    // Textfield Delegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTextField {
+            userNameTextField.resignFirstResponder()
+            dateOfBirthTextField.becomeFirstResponder()
+        }
+        else if textField == eMailtextField {
+            eMailtextField.resignFirstResponder()
+            placeOfBirthTextField.becomeFirstResponder()
+        }
+        else if textField == phoneNumberTextField {
+            phoneNumberTextField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == dateOfBirthTextField {
+            //dateOfBirthTextField.keyboardAppearance = false
+            dateOfBirthTextField.resignFirstResponder()
+            datePicker.isHidden = false
+        }else if textField == placeOfBirthTextField {
+            //placeOfBirthTextField.keyboardAppearance = false
+            placePicker.isHidden = false
+            placeOfBirthTextField.resignFirstResponder()
+            //phoneNumberTextField.becomeFirstResponder()
+        }
+    }
+    
+    //Picker Protocol
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return citiesName.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return citiesName[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        placeOfBirthTextField.text = citiesName[row]
+        placePicker.isHidden = true
+        phoneNumberTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func makeAccountPrivate() {
+        if privateAccountSwitch.isOn {
+            privateMsgLabel.text = "Your account will be kept Private"
+        }
+        else {
+            privateMsgLabel.text = "Your account won't be kept Private "
+        }
+    }
+    
+    //Submit Button functionality
     @IBAction func SignUpDone() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: "DescriptionController")
