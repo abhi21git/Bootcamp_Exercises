5d671df50f1dd43857599073ee82decfa0e7d56a
Abhishek Maurya <abhishek.maurya08@tothenew.com>
2019-03-15 22:08:16 +0530

    exercise 11

--- iOS/Exercise 11 Auto Layout/Exercise 11 Auto Layout/Question1.swift
+++ iOS/Exercise 11 Auto Layout/Exercise 11 Auto Layout/Question1.swift
@@ -9,11 +9,72 @@
 import UIKit
 
 class Question1: UIViewController {
-
+    
+    @IBOutlet var noOfElementsToShow: UITextField!
+    @IBOutlet var parentStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        noOfElementsToShow.becomeFirstResponder()
+        noOfElementsToShow.keyboardType = .numberPad
+                // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func ViewGenerator() {
+        
+        let noOfViews = Int(noOfElementsToShow.text!)
+        
+        let firstStackView = UIStackView()
+        firstStackView.axis = .horizontal
+        firstStackView.distribution = .fillEqually
+        
+        let secondStackView = UIStackView()
+        secondStackView.axis = .horizontal
+        secondStackView.distribution = .fillEqually
+        
+        let firstImageView = UIImageView()
+        firstImageView.backgroundColor = .blue
+        
+        let secondImageView = UIImageView()
+        secondImageView.backgroundColor = .yellow
+        
+        let thirdImageView = UIImageView()
+        thirdImageView.backgroundColor = .green
+        
+        let fourthImageView = UIImageView()
+        fourthImageView.backgroundColor = .red
+        
+        //to clear parent stack view so newer views don't get appended
+        for views in parentStackView.subviews{
+            views.removeFromSuperview()
+        }
+        
+        if noOfViews == 1 {
+            parentStackView.addArrangedSubview(firstImageView)
+            
+        }else if noOfViews == 2 {
+            parentStackView.addArrangedSubview(firstImageView)
+            parentStackView.addArrangedSubview(secondImageView)
+            
+        }else if noOfViews == 3 {
+            parentStackView.addArrangedSubview(firstImageView)
+            parentStackView.addArrangedSubview(secondImageView)
+            parentStackView.addArrangedSubview(thirdImageView)
+            
+        }else if noOfViews == 4 {
+            firstStackView.addArrangedSubview(firstImageView)
+            firstStackView.addArrangedSubview(secondImageView)
+            secondStackView.addArrangedSubview(thirdImageView)
+            secondStackView.addArrangedSubview(fourthImageView)
 
-        // Do any additional setup after loading the view.
+            parentStackView.addArrangedSubview(firstStackView)
+            parentStackView.addArrangedSubview(secondStackView)
+            
+        }else {
+            noOfElementsToShow.text = ""
+            
+        }
+        
     }
     
 
--- iOS/Exercise 11 Auto Layout/Exercise 11 Auto Layout/Question3.swift
+++ iOS/Exercise 11 Auto Layout/Exercise 11 Auto Layout/Question3.swift
@@ -9,13 +9,67 @@
 import UIKit
 
 class Question3: UIViewController {
+    
+    @IBOutlet var firstView: UIView!
+    @IBOutlet var firstImage: UIImageView!
+    @IBOutlet var firstLabel: UILabel!
+    @IBOutlet var secondView: UIView!
+    @IBOutlet var secondImage: UIImageView!
+    @IBOutlet var secondLabel: UILabel!
+    @IBOutlet var thirdView: UIView!
+    @IBOutlet var thirdImage: UIImageView!
+    @IBOutlet var thirdLabel: UILabel!
+    @IBOutlet var fourthView: UIView!
+    @IBOutlet var fourthImage: UIImageView!
+    @IBOutlet var fourthLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        autoLayoutProgramitically()
         // Do any additional setup after loading the view.
     }
     
+    func autoLayoutProgramitically() {
+//        let hieghtOfFrame = view.safeAreaLayoutGuide.layoutFrame.size.height
+//        let widthOfFrame = view.safeAreaLayoutGuide.layoutFrame.size.width
+        
+        //self.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        //first view
+        NSLayoutConstraint(item: firstView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: firstView, attribute: .left, relatedBy: .equal, toItem: self.view, attribute: .left, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: firstView, attribute: .height, relatedBy: .equal, toItem: self.secondView, attribute: .height, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: firstView, attribute: .height, relatedBy: .equal, toItem: self.secondView, attribute: .width, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: firstView, attribute: .height, relatedBy: .equal, toItem: self.thirdView, attribute: .height, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: firstView, attribute: .height, relatedBy: .equal, toItem: self.thirdView, attribute: .width, multiplier: 1, constant: 0).isActive = true
+        
+        //second view
+        NSLayoutConstraint(item: secondView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: secondView, attribute: .right, relatedBy: .equal, toItem: self.view, attribute: .right, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: secondView, attribute: .height, relatedBy: .equal, toItem: self.firstView, attribute: .height, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: secondView, attribute: .height, relatedBy: .equal, toItem: self.firstView, attribute: .width, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: secondView, attribute: .height, relatedBy: .equal, toItem: self.fourthView, attribute: .height, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: secondView, attribute: .height, relatedBy: .equal, toItem: self.fourthView, attribute: .width, multiplier: 1, constant: 0).isActive = true
+        
+        //third view
+        NSLayoutConstraint(item: thirdView, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: thirdView, attribute: .left, relatedBy: .equal, toItem: self.view, attribute: .left, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: thirdView, attribute: .height, relatedBy: .equal, toItem: self.fourthView, attribute: .height, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: thirdView, attribute: .height, relatedBy: .equal, toItem: self.fourthView, attribute: .width, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: thirdView, attribute: .height, relatedBy: .equal, toItem: self.firstView, attribute: .height, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: thirdView, attribute: .height, relatedBy: .equal, toItem: self.firstView, attribute: .width, multiplier: 1, constant: 0).isActive = true
+        
+        //fourth view
+        NSLayoutConstraint(item: fourthView, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: fourthView, attribute: .right, relatedBy: .equal, toItem: self.view, attribute: .right, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: fourthView, attribute: .height, relatedBy: .equal, toItem: self.thirdView, attribute: .height, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: fourthView, attribute: .height, relatedBy: .equal, toItem: self.thirdView, attribute: .width, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: fourthView, attribute: .height, relatedBy: .equal, toItem: self.secondView, attribute: .height, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: fourthView, attribute: .height, relatedBy: .equal, toItem: self.secondView, attribute: .width, multiplier: 1, constant: 0).isActive = true
+
+    }
 
     /*
     // MARK: - Navigation
