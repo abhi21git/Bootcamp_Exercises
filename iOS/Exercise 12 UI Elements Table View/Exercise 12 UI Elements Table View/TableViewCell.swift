8b83b1a39756a1b79a989d3d3a16185c22bceebe
Abhishek Maurya <abhishek.maurya08@tothenew.com>
2019-03-18 14:58:47 +0530

    nothing

---
+++ iOS/Exercise 12 UI Elements Table View/Exercise 12 UI Elements Table View/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Exercise 12 UI Elements Table View
+//
+//  Created by Abhishek Maurya on 18/03/19.
+//  Copyright © 2019 Abhishek Maurya. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ iOS/Exercise 12 UI Elements Table View/Exercise 12 UI Elements Table View/TableViewData.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exercise 12 UI Elements Table View
+//
+//  Created by Abhishek Maurya on 18/03/19.
+//  Copyright © 2019 Abhishek Maurya. All rights reserved.
+//
+
+import UIKit
+
+class TableViewData: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
