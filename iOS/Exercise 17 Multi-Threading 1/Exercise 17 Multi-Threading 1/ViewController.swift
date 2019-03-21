e33e51f4b351de700d42c2120adc6bd532420bf8
Abhishek Maurya <abhishek.maurya08@tothenew.com>
2019-03-22 01:21:33 +0530

    modified ex-17

--- iOS/Exercise 17 Multi-Threading 1/Exercise 17 Multi-Threading 1/ViewController.swift
+++ iOS/Exercise 17 Multi-Threading 1/Exercise 17 Multi-Threading 1/ViewController.swift
@@ -16,8 +16,9 @@ class ViewController: UIViewController ,UITableViewDelegate , UITableViewDataSou
     @IBOutlet var lapButton: UIButton!
     @IBOutlet var stopButton: UIButton!
     @IBOutlet var timeView: UIView!
+    @IBOutlet var backgroundImage: UIView!
 
-    var timer = Timer()
+    var timeTimer = Timer()
     var timeInMS = 0
     var timeInSec = 0
     var timeInMin = 0
@@ -39,54 +40,55 @@ class ViewController: UIViewController ,UITableViewDelegate , UITableViewDataSou
     }
     
     @IBAction func startAction() {
-        UIView.transition(with: startButton, duration: 0.5, options: .transitionCrossDissolve, animations: { self.startButton.isEnabled = false }, completion: nil)
-        UIView.transition(with: lapButton, duration: 0.5, options: .transitionCrossDissolve, animations: { self.lapButton.isEnabled = true }, completion: nil)
-        UIView.transition(with: stopButton, duration: 0.5, options: .transitionCrossDissolve, animations: { self.stopButton.isEnabled = true }, completion: nil)
         UIView.transition(with: timeView, duration: 0.5, options: .transitionFlipFromRight, animations: {}, completion: nil)
-
-        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(ViewController.incrementTimer), userInfo: nil, repeats: true)
+        
+        DispatchQueue.main.async {
+            self.timeTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(ViewController.incrementTimer), userInfo: nil, repeats: true)
+        }
+        
+        startButton.isEnabled = false
+        lapButton.isEnabled = true
+        stopButton.isEnabled = true
+        rotation(imageView: backgroundImage, oneRotationIn: 15.0)
     }
     
     @IBAction func lapAction() {
-        UIView.transition(with: lapButton, duration: 0.5, options: .transitionCrossDissolve, animations: {}, completion: nil)
         UIView.transition(with: timeView, duration: 0.5, options: .transitionFlipFromBottom, animations: {}, completion: nil)
-        UIView.transition(with: lapTable, duration: 0.5, options: .transitionCrossDissolve, animations: {
-                DispatchQueue.main.async {
-                    self.timeArray.insert(self.timeLabel.text!, at: 0)
-                    self.lapTable.reloadData()
-            }
-        }, completion: nil)
+        
+        DispatchQueue.main.async {
+            self.timeArray.insert(self.timeLabel.text!, at: 0)
+            self.lapTable.reloadData()
+        }
     }
     
     @IBAction func stopAction() {
-        UIView.transition(with: startButton, duration: 0.5, options: .transitionCrossDissolve, animations: { self.startButton.isEnabled = true }, completion: nil)
-        UIView.transition(with: lapButton, duration: 0.5, options: .transitionCrossDissolve, animations: { self.lapButton.isEnabled = false }, completion: nil)
-        UIView.transition(with: stopButton, duration: 0.5, options: .transitionCrossDissolve, animations: { self.stopButton.isEnabled = false }, completion: nil)
-        if timeArray.count == 0 {
-            //No animation required for table view
-        }else{
-            UIView.transition(with: lapTable, duration: 0.5, options: .transitionCrossDissolve, animations: {}, completion: nil)
-        }
-        UIView.transition(with: timeView, duration: 0.5, options: .transitionFlipFromLeft, animations: {
-            UIView.transition(with: self.timeLabel, duration: 0.5, options: .transitionCrossDissolve, animations: {self.timeLabel.text = "00:00:00"}, completion: nil)
-        }, completion: nil)
-        timer.invalidate()
+        startButton.isEnabled = true
+        lapButton.isEnabled = false
+        stopButton.isEnabled = false
+        
+        UIView.transition(with: timeView, duration: 0.5, options: .transitionFlipFromLeft, animations: {}, completion: nil)
+        
+        timeLabel.text = "00:00:00"
+        timeTimer.invalidate()
         timeInMS = 0
         timeInSec = 0
         timeInMin = 0
-        timeArray = []
+        timeArray.removeAll()
         lapTable.reloadData()
+        backgroundImage.layer.removeAllAnimations()
+
     }
     
     //Function to increase time
     @objc func incrementTimer() {
-        if(timeInMS <= 99) {
+        
+        if(timeInMS < 100) {
             timeLabel.text = String(format: "%02d:%02d:%02d", timeInMin, timeInSec, timeInMS)
             timeInMS += 1
         }
         else{
             timeInMS = 0
-            if(timeInSec <= 59) {
+            if(timeInSec < 60) {
                 timeInSec += 1
                 timeLabel.text = String(format: "%02d:%02d:%02d", timeInMin, timeInSec, timeInMS)            }
             else{
@@ -98,6 +100,16 @@ class ViewController: UIViewController ,UITableViewDelegate , UITableViewDataSou
         }
     }
     
+    //function for 360 rotation
+    func rotation(imageView: UIView, oneRotationIn: Double) {
+        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotationAnimation.fromValue = 0.0
+        rotationAnimation.toValue = Double.pi * 2
+        rotationAnimation.duration = oneRotationIn
+        rotationAnimation.repeatCount = .infinity
+        imageView.layer.add(rotationAnimation, forKey: nil)
+    }
+
     //Protocol for Table View data loading
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return timeArray.count
