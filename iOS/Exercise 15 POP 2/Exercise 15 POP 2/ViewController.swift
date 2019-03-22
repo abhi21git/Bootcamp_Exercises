71313dc4023a7fcec26deb64789867546ef7bc5d
Abhishek Maurya <abhishek.maurya08@tothenew.com>
2019-03-22 18:53:54 +0530

    added ex-15

---
+++ iOS/Exercise 15 POP 2/Exercise 15 POP 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exercise 15 POP 2
+//
+//  Created by Abhishek Maurya on 22/03/19.
+//  Copyright © 2019 Abhishek Maurya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
