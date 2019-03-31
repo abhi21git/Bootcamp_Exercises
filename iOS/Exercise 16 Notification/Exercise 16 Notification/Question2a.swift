09515ada002a39476c25cd45eb2841413e6eee7a
Abhishek Maurya <abhishek.maurya08@tothenew.com>
2019-04-01 01:26:04 +0530

    no message

---
+++ iOS/Exercise 16 Notification/Exercise 16 Notification/Question2a.swift
@@ -0,0 +1,36 @@
+//
+//  Question2a.swift
+//  Exercise 16 Notification
+//
+//  Created by Abhishek Maurya on 29/03/19.
+//  Copyright © 2019 Abhishek Maurya. All rights reserved.
+//
+
+import UIKit
+
+class Question2a: UIViewController {
+
+    @IBOutlet weak var pushButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func pushController() {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
