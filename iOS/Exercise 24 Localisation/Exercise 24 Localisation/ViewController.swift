4a2c1fedb700b5325bf1c55800936fde53ab7740
Abhishek Maurya <abhishek.maurya08@tothenew.com>
2019-04-10 20:25:04 +0530

    localisation

--- iOS/Exercise 24 Localisation/Exercise 24 Localisation/ViewController.swift
+++ iOS/Exercise 24 Localisation/Exercise 24 Localisation/ViewController.swift
@@ -36,7 +36,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        localisable()
     }
     
     //Mark:- Functions
