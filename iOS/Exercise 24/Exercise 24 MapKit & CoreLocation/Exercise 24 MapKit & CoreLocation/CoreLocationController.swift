cfa39758187752a210cb7f1112edd7d6814e5956
Abhishek Maurya <abhishek.maurya08@tothenew.com>
2019-04-12 13:23:49 +0530

    exercise 24

---
+++ iOS/Exercise 24/Exercise 24 MapKit & CoreLocation/Exercise 24 MapKit & CoreLocation/CoreLocationController.swift
@@ -0,0 +1,30 @@
+//
+//  CoreLocationController.swift
+//  Exercise 24 MapKit & CoreLocation
+//
+//  Created by Abhishek Maurya on 12/04/19.
+//  Copyright © 2019 Abhishek Maurya. All rights reserved.
+//
+
+import UIKit
+
+class CoreLocationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
